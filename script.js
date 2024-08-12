eafe651c1ccc839f9530080e1a26fde6bfc31faf
MD.Shojeb Hossain Shojol <70110235+shojebscodeplay@users.noreply.github.com>
2024-08-12 20:02:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
